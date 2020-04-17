65e0b5006e0a9ba83014f776f30b16db4f0c8f28
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-17 04:44:57 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,7 @@
 # Write your code below game_hash
+
+require "pry"
+
 def game_hash
   {
     home: {
@@ -126,4 +129,113 @@ def game_hash
   }
 end
 
-# Write code here
+###### -------------- # Write code here -----------
+
+def num_points_scored(players_name) 
+  
+  game_hash.each do |team_location, team_name_color_info|
+    team_name_color_info[:players].each do |player_data|
+      if (player_data[:player_name] == players_name)
+        return player_data[:points]
+      end
+    end
+  end 
+end
+
+def shoe_size(name_of_player) ------- #to not confuse myself with player_name key/value pair
+  
+  game_hash.each do |team_location, team_data| 
+
+    team_data[:players].each do | player_name |
+      if (player_name[:player_name] == name_of_player)
+        return player_name[:shoe]
+      end
+    end 
+  end 
+  
+end 
+
+
+def team_colors(team_name)
+  
+  game_hash.each do |team_location, team_name_color_info|
+      if team_name_color_info[:team_name] == team_name
+        return team_name_color_info[:colors]
+      end 
+  end 
+  
+end 
+
+def team_names
+  # --- returns array --- **
+  
+  team_names_arr = []
+  
+  game_hash.each do |team_location, team_name_color_info|
+     team_name_color_info.each do |team_name_key, team_name_value|
+       if team_name_key == :team_name
+         team_names_arr.push(team_name_value) #Note to self - pushing :team_names into empty array
+        end
+     end
+  end 
+  return team_names_arr
+end 
+
+def player_numbers(team_name)
+  jersey_numbers = []
+  
+  game_hash.each do |team_location, team_name_color_info|
+     if (team_name_color_info[:team_name] == team_name)
+        team_name_color_info.each do |team_info_keys, team_data|
+          if team_info_keys == :players
+            team_data.each do | player_data |
+              jersey_numbers.push(player_data[:number])
+            end 
+          end
+        end 
+      end 
+    end
+  return jersey_numbers
+end
+
+
+def player_stats(players_name)
+  
+  game_hash.each do |team_location, team_name_color_info|
+    team_name_color_info.each do |team_info_keys, team_data|
+      if team_info_keys == :players 
+        team_data.each do | player_data |
+          if (player_data[:player_name] == players_name)
+            player_data.delete([:player_name])
+            
+            ## have to remove the name from this kvp since it isn't asking for it in the return prompt
+            
+            return player_data
+          end
+        end
+      end
+    end
+  end
+end
+
+def big_shoe_rebounds 
+  
+  shoe_size = 0 
+  rebounds_value = 0
+  
+  game_hash.each do |team_location, team_name_color_info|
+    
+    team_name_color_info[:players].each do |player_data|
+      
+      if (player_data[:shoe] > shoe_size)
+          shoe_size = player_data[:shoe]
+
+          rebounds_value = player_data[:rebounds]
+      end 
+    end
+  end
+  return rebounds_value
+end #return number of rebounds with player with biggest shoe size
+
+ 
+## naming variables well, how to debug, "detective work", binding.pry = best friend
